29d2ca78fa92e20bc7880f8614b6512f9327cd4f
ML53413-Kizaqi <87198047+Kizaqi@users.noreply.github.com>
2022-12-09 11:52:51 +0200

    burger
    bugs

--- src/js/script.js
+++ src/js/script.js
@@ -17,49 +17,34 @@ $(document).ready(function(){
     })
     
   });
-  function burgerMenu(selector) {
-    let menu = $(selector);
-    let button = menu.find('.burger-menu_button', '.burger-menu_lines');
-    let links = menu.find('.burger-menu_link');
-    let overlay = menu.find('.burger-menu_overlay');
+function burgerMenu(selector) {
+  let menu = $(selector);
+  let button = menu.find('.burger-menu_button', '.burger-menu_lines');
+  let links = menu.find('.burger-menu_link');
+  let overlay = menu.find('.burger-menu_overlay');
+  
+  button.on('click', (e) => {
+    e.preventDefault();
+    toggleMenu();
+  });
+  
+  links.on('click', () => toggleMenu());
+  overlay.on('click', () => toggleMenu());
+  
+  function toggleMenu(){
+    menu.toggleClass('burger-menu_active');
     
-    button.on('click', (e) => {
-      e.preventDefault();
-      toggleMenu();
-    });
-    
-    links.on('click', () => toggleMenu());
-    overlay.on('click', () => toggleMenu());
-    
-    function toggleMenu(){
-      menu.toggleClass('burger-menu_active');
-      
-      if (menu.hasClass('burger-menu_active')) {
-        $('body').css('overlow', 'hidden');
-      } else {
-        $('body').css('overlow', 'visible');
-      }
+    if (menu.hasClass('burger-menu_active')) {
+      $('body').css('overlow', 'hidden');
+    } else {
+      $('body').css('overlow', 'visible');
     }
   }
-//   $('.content .owl-carousel ').owlCarousel({
-//     rtl:false,
-//     loop:true,
-//     margin:10,
-//     nav:true,
-//     dots:false,
-//     responsive:{
-//         0:{
-//             items:1
-//         },
-//         600:{
-//             items:3
-//         },
-//         1000:{
-//             items:2
-//         }
-//     }
-// }
-// );
+}
+
+burgerMenu('.burger-menu');
+
+
 
 // var owl = $('.owl-carousel');
 // owl.owlCarousel();
@@ -73,5 +58,4 @@ $(document).ready(function(){
 // })
 
   
-burgerMenu('.burger-menu');
   
